8792135595c99d8cbaf92baef6331c8247e646d8
Jakub Kruszyk <k.kruszyk7@gmail.com>
2025-01-18 22:26:56 +0100

    Recreated config on fork

--- init.lua
+++ init.lua
@@ -105,5 +105,13 @@ require 'lazy-bootstrap'
 -- [[ Configure and install plugins ]]
 require 'lazy-plugins'
 
+-- Gitsigns highlight colors
+
+vim.filetype.add {
+  extension = { v = 'verilog' },
+}
+
+vim.cmd.colorscheme 'catppuccin'
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/kickstart/plugins/cmp.lua
+++ lua/kickstart/plugins/cmp.lua
@@ -19,12 +19,21 @@ return {
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
+        },
+      },
+      {
+        'Saecki/crates.nvim',
+        event = { 'BufRead Cargo.toml' },
+        opts = {
+          completion = {
+            cmp = { enabled = true },
+          },
         },
       },
       'saadparwaiz1/cmp_luasnip',
@@ -34,7 +43,14 @@ return {
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
+      'hrsh7th/cmp-buffer',
     },
+
+    opts = function(_, opts)
+      opts.auto_brackets = opts.auto_brackets or {}
+      table.insert(opts.auto_brackets, 'python')
+    end,
+
     config = function()
       -- See `:help cmp`
       local cmp = require 'cmp'
@@ -110,6 +126,9 @@ return {
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'buffer' },
+          { name = 'nvim_lua' },
+          { name = 'crates' },
         },
       }
     end,
