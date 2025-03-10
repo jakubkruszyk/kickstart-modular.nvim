cdbf5f14c0510fe7c133c9b82e2ef73b81481667
Jakub Kruszyk <k.kruszyk7@gmail.com>
2025-03-10 21:51:06 +0100

    removed obsolete theme. added delay to which-key

--- init.lua
+++ init.lua
@@ -111,7 +111,7 @@ vim.filetype.add {
   extension = { v = 'verilog' },
 }
 
-vim.cmd.colorscheme 'catppuccin'
+vim.cmd.colorscheme 'catppuccin-mocha'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
